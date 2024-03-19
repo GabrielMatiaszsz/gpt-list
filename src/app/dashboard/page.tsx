e66e3d58a1559589367a309f4a2b62d36ae25006
Gabriel Matias <gmsilva@uniara.edu.br>
2024-03-19 09:12:16 -0300

    atualizando ambiente para backend

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -14,23 +14,28 @@ const DashboardPage = (props: Props) => {
                 <div className="max-w-7xl mx-auto p-10">
                     <div className="h-14"></div>
                     <div className="flex justify-between items-center md:flex-row flex-col">
-                      <div className='flex items-center'>
-                      <Link href="/">
-                <Button className="bg-green-600" size="sm">
-                  <ArrowLeft className="mr-1 w-4 h-4" />
-                  Back
-                </Button>
-              </Link>
-              <div className="w-4"></div>
-                    <h1 className='text-3xl font-bold text-gray-900'> Minhas notas </h1>
-                    <div className="w-4"></div>
-                    <UserButton/>
-                      </div>
+                        <div className='flex items-center'>
+                            <Link href="/">
+                                <Button className="bg-green-600" size="sm">
+                                    <ArrowLeft className="mr-1 w-4 h-4" />
+                                    Voltar
+                                </Button>
+                            </Link>
+                            <div className="w-4"></div>
+                            <h1 className='text-3xl font-bold text-gray-900'> Minhas notas </h1>
+                            <div className="w-4"></div>
+                            <UserButton />
+                        </div>
                     </div>
-                    <div className="h8"></div>
-                    <Separator/>
-                     <div className="h8"></div>
-               
+                    <div className="h-8"></div>
+                    <Separator />
+                    <div className="h-8"></div>
+                    {/* list all the notes */}
+
+                    <div className="text-center">
+                        <h2 className="text-xl text-gray-500">You have no notes yet.</h2>
+                    </div>
+
 
 
                 </div>
